6ad9597f248aed5b6d89ffc5aea7857efc88e3b2
AnthonyAl-Rifai <anthonyalrifai@gmail.com>
2023-03-21 12:40:18 -0500

    initial commit: set up nextjs project

---
+++ containers/Home/index.tsx
@@ -0,0 +1,13 @@
+import styles from '@/styles/Home.module.css';
+
+const Home = () => {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <h1>Hello World</h1>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
